1aab6ca0248cd77f1fb980d795e212c9fdf64691
Matthieu CASTET <matthieu.castet@parrot.com>
2012-04-13 17:24:39 +0200

    add back diag about missing jiffies

--- ltt2lxt/mod_kernel.c
+++ ltt2lxt/mod_kernel.c
@@ -608,9 +608,8 @@ static void kernel_timer_update_time_process(struct ltt_module *mod,
     if (pass == 2) {
 		static unsigned int old_jiffies;
 		if (old_jiffies && old_jiffies+1 != c_jiffies)
-            /* missing jiffies is not an error depending on the kernel config
-			DIAG("missing jiffies jump from %x to %x (broken trace ?)\n",
-				   old_jiffies, c_jiffies);*/
+			TDIAG(res, "missing jiffies jump from %x to %x (broken trace ?)\n",
+				   old_jiffies, c_jiffies);
 
 		old_jiffies = c_jiffies;
         emit_trace(&jiffies, (union ltt_value)c_jiffies);
